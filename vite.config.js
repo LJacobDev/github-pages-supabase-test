dc1a9c2d328003c49b2b16647e8a25786c60b73a
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2025-09-14 20:16:41 -0400

    fixing github pages render issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: "/github-pages-supabase-test/", // Set this to your repository name
+  build: {
+    outDir: "dist", // Ensure the output directory matches the GitHub Actions workflow
+  },
+});
